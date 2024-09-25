b0ce70a6a887d5207584ad0175a87afbb68373de
jheel23 <119886129+jheel23@users.noreply.github.com>
2024-09-25 19:37:53 +0530

    minor changes

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -42,6 +42,7 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     ref.watch(searchProvider);
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
@@ -50,50 +51,49 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
           _searchController.clear();
           ref.read(searchProvider.notifier).clearList();
         },
-        child: Scaffold(
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
-            title: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10)),
-              child: TextField(
-                controller: _searchController,
-                style: const TextStyle(color: Colors.white),
-                textInputAction: TextInputAction.search,
-                onSubmitted: (value) {
-                  FocusScope.of(context).unfocus();
-                  _searchMovies(value);
-                },
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Icon(
-                      Icons.search_rounded,
-                      color: Colors.grey,
-                    ),
-                    hintText: 'Search for movies, tv shows...',
-                    hintStyle: TextStyle(color: Colors.grey)),
-              ),
+        child: Column(children: [
+          SizedBox(
+            height: deviceSize.height * 0.03,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+            child: TextField(
+              controller: _searchController,
+              style: const TextStyle(color: Colors.white),
+              textInputAction: TextInputAction.search,
+              onSubmitted: (value) {
+                FocusScope.of(context).unfocus();
+                _searchMovies(value);
+              },
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  prefixIcon: Icon(
+                    Icons.search_rounded,
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Search for movies, tv shows...',
+                  hintStyle: TextStyle(color: Colors.grey)),
             ),
           ),
-          body: searchMovie.isEmpty
-              ? const Center(
-                  child: Text('Search for movies, tv shows...',
+          searchMovie.isEmpty
+              ? Container(
+                  height: deviceSize.height * 0.8,
+                  alignment: Alignment.center,
+                  child: const Text('Search for movies, tv shows...',
                       style: TextStyle(color: Colors.white)),
                 )
               : Container(
                   margin: const EdgeInsets.only(top: 10),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: List.generate(
-                        searchMovie.length,
-                        (index) => CustomSearchCard(movie: searchMovie[index]),
-                      ),
+                  child: Column(
+                    children: List.generate(
+                      searchMovie.length,
+                      (index) => CustomSearchCard(movie: searchMovie[index]),
                     ),
                   )),
-        ),
+        ]),
       ),
     );
   }
